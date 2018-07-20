ebc44773a8d276138c2bdbab1da2e33a4deb25c2
Snowflake is the best ! <vvsummer0928@gmail.com>
2018-07-20 11:29:55 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -21,7 +21,7 @@ WEBAPI_TOKEN = 'yuntuapi'
 MUDB_FILE = 'mudb.json'
 
 # Mysql
-MYSQL_HOST = '178.128.176.9'
+MYSQL_HOST = 'az01.uswest.rds.codec-cluster.org'
 MYSQL_PORT = 3306
 MYSQL_USER = 'yuntu'
 MYSQL_PASS = 'ss'
